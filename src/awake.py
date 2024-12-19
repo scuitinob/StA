0f4bebcfdc21077d1d01c5a5001d12fd5f231d07
Sergio Cuitiño <sergio.cuitino@avattar.com>
2024-12-19 15:12:09 -0300

    mejorando procedimientos

--- src/awake.py
+++ src/awake.py
@@ -11,9 +11,9 @@ else:
 while(True):
     x=0
     while(x<numMin):
-        time.sleep(60)
+        time.sleep(5)
         x+=1
-    for i in range(0,200):
+    for i in range(0,15):
         pyautogui.moveTo(0,i*4)
     pyautogui.moveTo(1,1)
     for i in range(0,3):
